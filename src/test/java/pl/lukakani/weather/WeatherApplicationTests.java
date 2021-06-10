12233391182235400c7acf2ee83b964ae22de686
Lukasz K <kanicki.lukasz@gmail.com>
2021-06-10 19:50:11 +0200

    initial commit

---
+++ src/test/java/pl/lukakani/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lukakani.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
